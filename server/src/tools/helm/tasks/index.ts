646ad4059e4e89f94449dd29db07a10c2aac47fe
mrmicaiah <mrmicaiah@gmail.com>
2026-01-28 21:23:32 -0600

    Add tasks index

---
+++ server/src/tools/helm/tasks/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Task Tools Index
+ */
+
+export { registerListTask } from './list.js';
+export { registerAddTask } from './add.js';
+export { registerCompleteTask } from './complete.js';
+export { registerUpdateTask } from './update.js';
+export { registerDeleteTask } from './delete.js';
+export { registerSnoozeTask } from './snooze.js';
+export { registerClaimTask } from './claim.js';
+export { registerBreakdownTask } from './breakdown.js';
