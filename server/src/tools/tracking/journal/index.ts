3d7905b0a46cc87850dd6aa5209c88265eb1da6d
mrmicaiah <mrmicaiah@gmail.com>
2026-01-28 22:52:11 -0600

    Add journal index

---
+++ server/src/tools/tracking/journal/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Journal Tools - Index
+ */
+
+export { registerJournalEntryTools } from './entries.js';
+export { registerJournalInsightTools } from './insights.js';
+export { registerJournalConfigTools } from './config.js';
